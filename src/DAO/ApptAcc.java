92f84b172709c6160267fefac2f99a972e165e89
EmilyB78 <emilybecherrn@yahoo.com>
2022-05-27 20:44:53 -0700

    Third Save

---
+++ src/DAO/ApptAcc.java
@@ -0,0 +1,61 @@
+package DAO;
+
+import Main.DataBaseCon;
+import Model.Appointments;
+import Model.Countries;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+
+public class ApptAcc {
+
+
+    public static ObservableList<Appointments> getAllAppointments(){
+
+        ObservableList<Appointments> alist = FXCollections.observableArrayList();
+
+        try {
+            String sql  = "SELECT * from appointments";
+
+            PreparedStatement ps = DataBaseCon.getConnection().prepareStatement(sql);
+
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                int appointmentID = rs.getInt("Appointment_ID");
+                String appointmentTitle = rs.getString("Title");
+                String appointmentDescription = rs.getString("Description");
+                String appointmentLocation = rs.getString("Location");
+                String appointmentType = rs.getString("Type");
+                LocalDateTime start = rs.getTimestamp("Start").toLocalDateTime();
+                LocalDateTime end = rs.getTimestamp("End").toLocalDateTime();
+                int customerID = rs.getInt("Customer_ID");
+                int userID = rs.getInt("User_ID");
+                int contactID = rs.getInt("Contact_ID");
+                Appointments A = new Appointments(appointmentID, appointmentTitle, appointmentDescription, appointmentLocation, appointmentType, start, end, customerID, userID, contactID);
+                alist.add(A);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return alist;
+    }
+    public static int deleteAppointment(int appointmentID, Connection connection) {
+
+        try {
+            String sql = "DELETE FROM appointments where Appointment_ID =? ";
+            PreparedStatement ps = DataBaseCon.getConnection().prepareStatement(sql);
+            ps.setInt(1, appointmentID);
+            int result = ps.executeUpdate();
+            ps.close();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return result;
+    }
+
+}
---
+++ src/DAO/UserAcc.java
@@ -0,0 +1,65 @@
+package DAO;
+
+import Main.DataBaseCon;
+import Model.Countries;
+import Model.User;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+public class UserAcc {
+
+    public static int validateUser(String userName, String userPassword ){
+
+        try {
+            String sql = "SELECT * FROM users WHERE user_name = '" + userName + "' AND password = '" + userPassword + "'";
+
+            PreparedStatement ps = DataBaseCon.getConnection().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            rs.next();
+            if (rs.getString("User_Name").equals(userName)) {
+                if (rs.getString("Password").equals(userPassword)) {
+                    return rs.getInt("User_ID");
+                }
+            }
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+
+        }
+        return -1;
+    }
+
+
+    public static ObservableList<User> getAllUsers(){
+
+        ObservableList<User> ulist = FXCollections.observableArrayList();
+
+        try {
+            String sql  = "SELECT * from users";
+
+            PreparedStatement ps = DataBaseCon.getConnection().prepareStatement(sql);
+
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                int userId = rs.getInt("User_ID");
+                String userName = rs.getString("User_Name");
+                String userPassword = rs.getString("Password");
+                User U = new User (userId, userName, userPassword);
+                ulist.add(U);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return ulist;
+    }
+
+
+}
