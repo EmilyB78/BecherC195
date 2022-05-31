2aeab97c8b56b2f5fe8614cd1dddd40386c9ae64
EmilyB78 <emilybecherrn@yahoo.com>
2022-05-30 17:56:22 -0700

    sixth save

--- src/Model/FirstLevelDiv.java
+++ src/Model/FirstLevelDiv.java
@@ -1,4 +1,35 @@
 package Model;
 
 public class FirstLevelDiv {
+    private int divisionID;
+    private String divisionName;
+    public int country_ID;
+
+    public FirstLevelDiv(int divisionID, String divisionName, int country_ID){
+        this.divisionID = divisionID;
+        this.divisionName = divisionName;
+        this.country_ID = country_ID;
+    }
+
+    public int getDivisionID() {
+        return divisionID;
+    }
+
+    public void setDivisionID(int divisionID) {
+        this.divisionID = divisionID;
+    }
+    public String getDivisionName(){
+        return divisionName;
+    }
+    public void setDivisionName(String divisionName) {
+        this.divisionName = divisionName;
+    }
+
+    public int getCountry_ID() {
+        return country_ID;
+    }
+
+    public void setCountry_ID(int country_ID) {
+        this.country_ID = country_ID;
+    }
 }
